513d7356f652317ff9b068ea800c532a9329d758
Alessandra Santos Dias <alessandrasdiastrabalho@gmail.com>
2022-11-14 18:31:02 -0300

    Templates html files + func loadTemplates

--- Party_invites/main.go
+++ Party_invites/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
 
-//entry point of the app
+type Rsvp struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+// collect Rsvp values together.
+// make initializes a new slice, last two arguments are initial size and initial capacity 0= empty slice
+var responses = make([]*Rsvp, 0, 10)
+var templates = make(map[string]*template.Template, 3)
+
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for index, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+
+// entry point of the app
 func main() {
 
-	fmt.Println("TODO: add some features")
+	loadTemplates()
 }
