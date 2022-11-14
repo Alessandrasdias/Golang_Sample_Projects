71d2409cf3821dc4f9f07b8338d25f740dcb50a9
Alessandra Santos Dias <alessandrasdiastrabalho@gmail.com>
2022-11-14 19:07:07 -0300

    HTTP Handlers and Server - Welcome and List

--- Party_invites/main.go
+++ Party_invites/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"html/template"
+
+	//functionality for dealing with HTTP requests
+	"net/http"
 )
 
 type Rsvp struct {
@@ -15,6 +18,8 @@ type Rsvp struct {
 var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
+// will be responsible for loading the HTML files defined in earlier listings
+// and processing them to create the *template.Template values that will be stored in the map
 func loadTemplates() {
 	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
 	for index, name := range templateNames {
@@ -28,8 +33,29 @@ func loadTemplates() {
 	}
 }
 
+//HTTP Handlers and Server
+//user requests the default URL
+//pointer to an instance of the Request struct. Request being processed.
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
+
 // entry point of the app
 func main() {
 
 	loadTemplates()
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
+
+	//create and http server that listens for requests on port 5000
+	// nil is to tell the server that requests should be processed using funcs registered with HandleFunc
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
