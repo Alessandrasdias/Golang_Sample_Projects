7e21baf0be276482c9e2c6194a1a83aa9533abe2
Alessandra Santos Dias <alessandrasdiastrabalho@gmail.com>
2022-12-02 15:30:43 -0300

    More examples

--- firstLetter/main.go
+++ firstLetter/main.go
@@ -50,3 +50,47 @@ func main() {
 	ch := repeatedCharacter("abccbaacz")
 	fmt.Println(string(ch))
 }
+
+// If it were for numbers
+//  find the duplicate and compare their index return the lower index
+// the first duplicate
+// if there are no elements return -1
+// hash map
+// numbSet:= map[int]int{}
+// rep:= 0
+// range the array
+// if i find inside my map i'll say that rep repeats first and I'll return it
+// if I don't find some one similar and my Array is not over I add that num to my set
+// is the array is over i return -1
+
+/*func solution(a []int) int {
+ numbSet:= map[int]int{}
+ rep:= 0
+  for i, num := range a{
+      if _, found := numbSet[num]; found{
+          rep = i
+          return a[rep]
+      }
+      numbSet[num] = i
+  }
+  return -1
+}*/
+
+// return de non repeating one
+// if all repeat return '_'
+// hashmap
+
+/*func solution(s string) string {
+	charSet :=  map[rune]uint{}
+	   for _, char := range s {
+		   charSet[char]++
+	   }
+
+	   for _, char := range s {
+		   if charSet[char] == 1 {
+			   return string(char)
+		   }
+	   }
+	   return "_"
+   }
+*/
